b275bcb6d8a337b114cef82ea384bce95cca63ac
Olivier Mbayi <olimbayi@gmail.com>
2025-04-12 19:37:02 +0200

    develop

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
 def format_linter_error(error: dict) -> dict:
     # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": erro["column_number"],
+        "message": error["name"],
+        "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     # write your code here
-    pass
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+         "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
     # write your code here
-    pass
+    return [
+        format_linter_report_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    ]
+
+print(format_linter_report(linter_report))
+    
